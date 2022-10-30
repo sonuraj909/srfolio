0531bc1abfab520fbf5aaf5b15075467c7d90267
Sonu Raj <sonuraj909@gmail.com>
2022-10-30 13:09:01 +0530

    initial commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+const kHeight = SizedBox(
+  height: 10,
+);
---
+++ lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
