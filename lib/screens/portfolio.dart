c9fc9993fe32fc635e14564eb7e92b83dd7afc07
Sonu Raj <sonuraj909@gmail.com>
2022-10-31 20:22:34 +0530

    portfolio work not complete yet still on progress

--- lib/screens/portfolio.dart
+++ lib/screens/portfolio.dart
@@ -13,54 +13,171 @@ class Portfolio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            TextWidget(
-              title: 'What I Do ?',
-              fsize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-            TextWidget(
-              title: 'I may not be perfect but surely i\'m of some use :-)',
-              fsize: 16,
-            ),
-            CarouselSlider(
-              items: [
-                cardWidget(
-                  child: TextWidget(
-                    fsize: 20,
-                    title: 'Android\nApplication\nDevelopment',
-                  ),
-                ),
-                cardWidget(
-                  child: TextWidget(
-                    fsize: 20,
-                    title: 'Web\nApplication\nDevelopment',
-                  ),
-                ),
-                cardWidget(
-                  child: TextWidget(
-                    fsize: 20,
-                    title: 'StatisticaAnalysis',
-                  ),
-                ),
-              ],
-              options: CarouselOptions(
-                height: 200,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20, left: 15, right: 10),
+          child: Column(
+            children: [
+              TextWidget(
+                title: 'What I Do ?',
+                fsize: 30,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-          ],
+              TextWidget(
+                title: 'I may not be perfect but surely i\'m of some use :-)',
+                fsize: 18,
+              ),
+              kHeight,
+              CarouselSlider(
+                items: [
+                  cardWidget(
+                    child: TextWidget(
+                      fsize: 20,
+                      title: 'Android\nApplication\nDevelopment',
+                    ),
+                  ),
+                  cardWidget(
+                    child: TextWidget(
+                      fsize: 20,
+                      title: 'Web\nApplication\nDevelopment',
+                    ),
+                  ),
+                  cardWidget(
+                    child: TextWidget(
+                      fsize: 20,
+                      title: 'StatisticaAnalysis',
+                    ),
+                  ),
+                ],
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              kHeight,
+              TextWidget(
+                title: 'Portfolio',
+                fsize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              TextWidget(
+                title: 'Here are few samples of my previous work :-)',
+                fsize: 18,
+              ),
+              kHeight,
+              CarouselSlider(
+                items: [
+                  cardWidget(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.money,
+                          size: 30,
+                        ),
+                        TextWidget(
+                          fsize: 18,
+                          title: 'Money Manager App',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        TextWidget(
+                          fsize: 16,
+                          title:
+                              'Personal finance management app for expense and income tracking',
+                        ),
+                      ],
+                    ),
+                  ),
+                  cardWidget(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.stream_rounded,
+                          size: 30,
+                        ),
+                        TextWidget(
+                          fsize: 18,
+                          title: 'Atom',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        TextWidget(
+                          fsize: 16,
+                          title:
+                              'Tried to clone the famous OTT platform Netflix. It\'s basically in-app not exactly like orginal App.',
+                        ),
+                      ],
+                    ),
+                  ),
+                  cardWidget(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.music_note_rounded,
+                          size: 30,
+                        ),
+                        TextWidget(
+                          fsize: 18,
+                          title: 'Sonics',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        TextWidget(
+                          fsize: 16,
+                          title:
+                              'It\'s basically a music player currently still work in progress.',
+                        ),
+                      ],
+                    ),
+                  ),
+                  cardWidget(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.notifications,
+                          size: 30,
+                        ),
+                        TextWidget(
+                          fsize: 18,
+                          title: 'Flood Monitoring System',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        TextWidget(
+                          fsize: 16,
+                          title:
+                              'It\'s my college final year project.Flood prediction using different sensor and Raspberry Pi.',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
